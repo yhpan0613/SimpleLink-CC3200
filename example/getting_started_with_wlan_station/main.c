86b563c365b9dd2af41904bb93b9d65dead4fd96
Yonghua Pan <yonghua-pan@ti.com>
2015-06-29 14:16:09 +0800

    Added wlan event for AP mode.
    Signed-off-by: Yonghua Pan <yonghua-pan@ti.com>

--- example/getting_started_with_wlan_station/main.c
+++ example/getting_started_with_wlan_station/main.c
@@ -366,6 +366,40 @@ void SimpleLinkWlanEventHandler(SlWlanEvent_t *pWlanEvent)
             memset(g_ucConnectionBSSID,0,sizeof(g_ucConnectionBSSID));
         }
         break;
+		
+        case SL_WLAN_STA_CONNECTED_EVENT:
+        {
+            // when device is in AP mode and any client connects to device cc3xxx
+            SET_STATUS_BIT(g_ulStatus, STATUS_BIT_CONNECTION);
+
+            //
+            // Information about the connected client (like SSID, MAC etc) will be
+            // available in 'slPeerInfoAsyncResponse_t' - Applications
+            // can use it if required
+            //
+            // slPeerInfoAsyncResponse_t *pEventData = NULL;
+            // pEventData = &pSlWlanEvent->EventData.APModeStaConnected;
+            //
+
+        }
+        break;
+
+        case SL_WLAN_STA_DISCONNECTED_EVENT:
+        {
+            // when client disconnects from device (AP)
+            CLR_STATUS_BIT(g_ulStatus, STATUS_BIT_CONNECTION);
+            CLR_STATUS_BIT(g_ulStatus, STATUS_BIT_IP_LEASED);
+
+            //
+            // Information about the connected client (like SSID, MAC etc) will
+            // be available in 'slPeerInfoAsyncResponse_t' - Applications
+            // can use it if required
+            //
+            // slPeerInfoAsyncResponse_t *pEventData = NULL;
+            // pEventData = &pSlWlanEvent->EventData.APModestaDisconnected;
+            //            
+        }
+        break;
 
         default:
         {
